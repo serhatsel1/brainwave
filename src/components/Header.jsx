04ad60b9ae65fbe3070e42dd5b593ebe2154ba02
Serhat Sel <selserhat01@gmail.com>
2024-03-25 01:35:11 +0300

    signup, signin in and nav added to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
+
+import { navigation } from "../constants";
 import { brainwave } from "../assets";
+import Button from "./Button";
+
 const Header = () => {
+  const pathName = useLocation();
   return (
     <div
-      className="fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b
+      className="fixed w-full left-0 top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b
        border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm"
     >
       <div
@@ -17,11 +23,40 @@ const Header = () => {
           className="hidden fixed top-[5rem] left-0 right-0 bottom-0
          bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent"
         >
-          <div className="relative z-2 flex flex-col items-center justify-center
-           m-auto lg:flex-row">
-            
+          <div
+            className="relative z-2 flex flex-col items-center justify-center
+           m-auto lg:flex-row"
+          >
+            {navigation.map((item) => (
+              <a
+                key={item.id}
+                href={item.url}
+                className={`block realtive font-code
+              text-2xl uppercase text-n-1 transition-colors
+              hover:text-color-1 ${
+                item.onlyMobile ? "lg:hidden" : ""
+              } px-6 py-6 md:py-8 
+              lg:-mr-0.25 lg:text-xs lg:font-semibold
+              ${
+                item.url === pathName.hash
+                  ? "z-2 lg:text-n-1"
+                  : "lg:text-n-1/50"
+              } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
+              >
+                {item.title}
+              </a>
+            ))}
           </div>
         </nav>
+        <a
+          href="#signup"
+          className="button hidden mr-8 text-n-1/50
+        transition-colors hover:text-n-1 lg:block
+        "
+        >New Account</a>
+        <Button className="hidden lg:flex" href="#login">
+              Sign in
+        </Button>
       </div>
     </div>
   );
