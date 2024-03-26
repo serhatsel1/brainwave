637bdda2cdaa5da6aec42d6d20cbf687b7f093c0
Serhat Sel <selserhat01@gmail.com>
2024-03-26 23:41:05 +0300

    header component editited : scroll-lock

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { useLocation } from "react-router-dom";
+import { disablePageScroll, enablePageScroll } from "scroll-lock";
 
 import { brainwave } from "../assets";
 import { navigation } from "../constants";
@@ -14,11 +15,16 @@ const Header = () => {
 
   const toggleNavigation = () => {
     setOpenNavigation(!openNavigation);
+    let temp = openNavigation;
+
+    temp ? enablePageScroll() : disablePageScroll();
   };
 
   const handleClick = () => {
-    setOpenNavigation(false)
-  }
+    if (!openNavigation) return;
+    enablePageScroll();
+    setOpenNavigation(false);
+  };
 
   return (
     <div
