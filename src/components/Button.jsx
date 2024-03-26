ef8ffa4b9ab76ab170e2f9290cb4c2a6a7ecf416
Serhat Sel <selserhat01@gmail.com>
2024-03-26 23:35:26 +0300

    navbar menu made responsive and functional button added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,14 +7,14 @@ const Button = ({ className, href, onClick, children, px, white }) => {
 
   const spanClasses = `relative z-10`;
   const renderButton = () => (
-    <button className={classes}>
+    <button className={classes} onClick={onClick}>
       <span className={spanClasses}>{children}</span>
       {ButtonSvg(white)}
     </button>
   );
 
   const renderLink = () => (
-    <a href={href} className={classes}>
+    <a href={href} className={classes}  onClick={onClick}>
       <span className={spanClasses}>{children}</span>
       {ButtonSvg(white)}
     </a>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,40 @@
-import React from "react";
 import { useLocation } from "react-router-dom";
 
-import { navigation } from "../constants";
 import { brainwave } from "../assets";
+import { navigation } from "../constants";
+
 import Button from "./Button";
+import MenuSvg from "../assets/svg/MenuSvg";
+import { HamburgerMenu } from "./design/Header";
+import { useState } from "react";
 
 const Header = () => {
-  const pathName = useLocation();
+  const pathname = useLocation();
+  const [openNavigation, setOpenNavigation] = useState(false);
+
+  const toggleNavigation = () => {
+    setOpenNavigation(!openNavigation);
+  };
+
+  const handleClick = () => {
+    setOpenNavigation(false)
+  }
+
   return (
     <div
-      className="fixed w-full left-0 top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b
-       border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm"
+      className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+        openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
+      }`}
     >
-      <div
-        className="flex items-center px-5 lg:px-7.5 xl:px-10
-       max-lg:py-4"
-      >
+      <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
           <img src={brainwave} width={190} height={40} alt="Brainwave" />
         </a>
         <nav
-          className="hidden fixed top-[5rem] left-0 right-0 bottom-0
-         bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent"
+          className={`${
+            openNavigation ? "flex" : "hidden"
+          }  fixed top-[5rem] left-0 right-0 bottom-0
+         bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
         >
           <div
             className="relative z-2 flex flex-col items-center justify-center
@@ -31,14 +44,15 @@ const Header = () => {
               <a
                 key={item.id}
                 href={item.url}
-                className={`block realtive font-code
+                onClick={handleClick}
+                className={`block relative font-code
               text-2xl uppercase text-n-1 transition-colors
               hover:text-color-1 ${
                 item.onlyMobile ? "lg:hidden" : ""
               } px-6 py-6 md:py-8 
               lg:-mr-0.25 lg:text-xs lg:font-semibold
               ${
-                item.url === pathName.hash
+                item.url === pathname.hash
                   ? "z-2 lg:text-n-1"
                   : "lg:text-n-1/50"
               } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
@@ -46,6 +60,7 @@ const Header = () => {
                 {item.title}
               </a>
             ))}
+            <HamburgerMenu />
           </div>
         </nav>
         <a
@@ -53,9 +68,18 @@ const Header = () => {
           className="button hidden mr-8 text-n-1/50
         transition-colors hover:text-n-1 lg:block
         "
-        >New Account</a>
+        >
+          New Account
+        </a>
         <Button className="hidden lg:flex" href="#login">
-              Sign in
+          Sign in
+        </Button>
+        <Button
+          className="ml-auto lg:hidden"
+          px="px-3"
+          onClick={toggleNavigation}
+        >
+          <MenuSvg openNavigation={openNavigation} />
         </Button>
       </div>
     </div>
